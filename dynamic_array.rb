1740eb47cc75b5e9ee354899a1c3154b8cac1b62
Zachary M Reisman <zach.reisman@gmail.com>
2015-07-20 20:51:48 -0700

    Implement dynamic array class

--- dynamic_array.rb
+++ dynamic_array.rb
@@ -1,7 +1,5 @@
 class StaticArray
 
-  attr_reader :length
-
   def initialize(capacity = 4)
     @capacity = capicity
     @store = Array.new(@capacity)
@@ -11,7 +9,7 @@ class StaticArray
     if idx < 0 || idx >= @length
       return nil
     else
-      return @store[idx]
+      return @store.get(idx)
     end
   end
 
@@ -19,15 +17,65 @@ class StaticArray
     if idx < 0 || idx >= @length
       return nil
     else
-      @store[idx] = value
+      @store.set(idx, value)
       return value
     end
   end
 
-
-
+  def length
+    @capacity
+  end
 end
 
 class DynamicArray
   def initialize
-    @store = Static
+    @store = StaticArray.new(4)
+    @capacity = 4
+    @length = 0
+  end
+
+  def get(idx)
+    return @store.get(idx) if inbounds?(idx)
+  end
+
+  def set(idx, value)
+    if inbounds?(idx)
+      @store.set(idx, value)
+    end
+  end
+
+  def push(value)
+    if @length < @capacity
+      add_stuff!(value)
+    else
+      expand_array
+      add_stuff!(value)
+    end
+
+  end
+
+  def pop
+    @length -= 1
+    get(@length)
+  end
+
+
+  private
+
+  def add_stuff!(value)
+    @store[@length] = value
+    @length += 1
+  end
+
+  def expand_array
+    old_store = @store
+    @capacity *= 2
+    @store = StaticArray.new(@capacity)
+    (0...@length).each {|i| @store.set(i, old_store.get(i)) }
+  end
+
+  def inbounds?(idx)
+    return true if idx < @length && idx >= 0
+  end
+
+end
